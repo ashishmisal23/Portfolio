b880b4b91e8d798a48f465811e3f77c032b7f320
ashishmisal23 <ashishmisal23@gmail.com>
2024-04-16 02:23:53 +0530

    Commited

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Comming <code>Soon</code> Ashish Misal Portfolio
-        </p>
-        
+        <img src={logo} className="App-logo" alt="logo" width="200px" />
+        <h2> Comming Soon, </h2>
+        <h1>
+          Ashish Misal's <br /> Portfolio
+        </h1>
+
       </header>
     </div>
   );
